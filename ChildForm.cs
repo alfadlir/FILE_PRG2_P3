a2275ebb00042a54f87147debf383ea0c2e83de2
Alfadli <alfadlirt@gmail.com>
2020-04-30 14:53:15 +0700

    Initial commit

---
+++ ChildForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG2_20192_P3_2_05
+{
+    public partial class Child_Form : Form
+    {
+        public Child_Form()
+        {
+            InitializeComponent();
+        }
+
+        String outputText;
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void DisplayButton_Click(object sender, EventArgs e)
+        {
+            OutputLabel.Text = outputText;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            outputText = DateTime.Now.ToString("d");
+        }
+
+        private void TimeOption_CheckedChanged(object sender, EventArgs e)
+        {
+            outputText = DateTime.Now.ToString("t");
+        }
+    }
+}
---
+++ ParentForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG2_20192_P3_2_05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            Child_Form newChild = new Child_Form();
+            newChild.MdiParent = this;
+            newChild.Show();
+
+        }
+    }
+}
